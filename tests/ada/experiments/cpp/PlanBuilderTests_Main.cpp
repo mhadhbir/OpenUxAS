8d7bcc178978c2571c0c38215757954d742dd1d0
M. Anthony Aiello <m.anthony.aiello@gmail.com>
2024-07-16 17:04:01 +0000

    Working build of the plan builder C++ testing.

--- tests/ada/experiments/cpp/PlanBuilderTests_Main.cpp
+++ tests/ada/experiments/cpp/PlanBuilderTests_Main.cpp
@@ -1,9 +1,15 @@
 #include "PlanBuilderTests.hpp"
 
+#include <iostream>
+
 int
 main(int argc, char** argv)
 {
+    std::cerr << "About to allocate tests" << std::endl;
     uxas::service::test::PlanBuilderTests tests;
+    std::cerr << "About to call first test" << std::endl;
     tests.Process_Task_Assignment_Summary_Test();
+    std::cerr << "About to call second test" << std::endl;
     tests.Process_Task_Implementation_Response_Vehicle_Exists_WPList_Empty_Test();
+    std::cerr << "Done" << std::endl;
 };
